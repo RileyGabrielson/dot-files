aebb43a13edfe334e8bcc5fee03503407be4e39a
Riley <riley.gabrielson@tcnbroadcasting.com>
2023-02-06 23:09:44 -0700

    typescript mason config

--- nvim/lua/lsp_servers/mason.lua
+++ nvim/lua/lsp_servers/mason.lua
@@ -41,3 +41,23 @@ for _, server in pairs(servers) do
   lspconfig[server].setup(opts)
 end
 
+require'typescript'.setup {
+  server = {
+    filetypes = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"},
+    root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    settings = { documentFormatting = false },
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = "non-relative",
+      },
+    },
+    handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+        })
+    },
+  }
+}
