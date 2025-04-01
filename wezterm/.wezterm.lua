ef6ea895a337488319cde9b1167a5fd9c7344b07
Riley <riley.gabrielson@tcnbroadcasting.com>
2025-04-01 00:35:12 -0700

    wezterm formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.font_size = 17.2
@@ -9,25 +9,25 @@ config.audible_bell = "Disabled"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW"
 config.window_padding = {
-  left = 2,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 2,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
-config.color_scheme = 'GruvboxDarkHard'
+config.color_scheme = "GruvboxDarkHard"
 
 config.background = {
-  {
-    source = {
-      File = "/Users/riley.gabrielson/lotr-background.jpeg",
-    },
-    height = "100%",
-    attachment = "Fixed",
-    repeat_x = "Mirror",
-    horizontal_align = "Right"
-  }
+	{
+		source = {
+			File = "/Users/riley.gabrielson/lotr-background.jpeg",
+		},
+		height = "100%",
+		attachment = "Fixed",
+		repeat_x = "Mirror",
+		horizontal_align = "Right",
+	},
 }
-config.font = wezterm.font 'JetBrains Mono'
+-- config.font = wezterm.font 'JetBrains Mono'
 config.check_for_updates = true
 
 return config
